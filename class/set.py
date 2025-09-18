66c5713b5fc5f36cb2bb8fd3bb6415be1e6507b3
AjayExplorer <161129605+AjayExplorer@users.noreply.github.com>
2025-09-18 15:52:35 +0530

    add

--- class/set.py
+++ class/set.py
@@ -3,3 +3,13 @@
 #add element by using add() function in set
 #set can creae by using set() function or by using {}
 a=set()
+n=int(input("enter the number of elements:"))
+print("enter the elements:")
+for i in range(0,n):
+    no=int(input())
+    a.add(no)
+print(a)
+s=0
+for x in a:
+    s=s+i
+print("sum of elements in the set is:",s)
